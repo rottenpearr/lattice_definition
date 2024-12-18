378d1896e78514a915f9ca85bce046f21f7ce56e
mruax <matvey.chernyakov@yandex.ru>
2024-12-18 21:29:37 +0500

    Все файлы читаются

--- db/data_to_db.py
+++ db/data_to_db.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import subprocess
+from glob import glob
 
 
 def parse_txt(lattice_type_id_path):
@@ -8,16 +9,20 @@ def parse_txt(lattice_type_id_path):
     return id
 
 
-def main():  # Здесь сделать 1 по имени или все через glob????  xyz_files_path = Path("../data/xyz")
-    json_file_path = Path("../data/json/1000041.json")
-    lattice_type_id_path = Path("lattice_type_id.txt")
-    xyz_file_path = Path("../data/xyz/1000041.xyz")
+def main():
+    files_path = Path("../data/json")
+    files = glob(str(files_path / "*.json"))
+    for file_path in files:
+        filename = Path(file_path).stem
+        json_file_path = Path(f"../data/json/{filename}.json")
+        lattice_type_id_path = Path("lattice_type_id.txt")
+        xyz_file_path = Path(f"../data/xyz/{filename}.xyz")
 
-    subprocess.run(["python", Path("json_to_db.py"), json_file_path])
+        subprocess.run(["python", Path("json_to_db.py"), json_file_path])
 
-    lattice_type_id = parse_txt(lattice_type_id_path)
+        lattice_type_id = parse_txt(lattice_type_id_path)
 
-    subprocess.run(["python", Path("xyz_to_db.py"), xyz_file_path, str(lattice_type_id)])
+        subprocess.run(["python", Path("xyz_to_db.py"), xyz_file_path, str(lattice_type_id)])
 
 if __name__ == "__main__":
     main()
