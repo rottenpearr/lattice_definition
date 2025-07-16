a74858484b5feb7fe384d299ecc2c828ec14a787
A1r3t0 <113704274+A1r3t0@users.noreply.github.com>
2025-07-16 19:48:26 +0500

    deleted example

--- db/normalize.py
+++ db/normalize.py
@@ -1,19 +1,7 @@
 import numpy as np
 
-# vec1 = [
-#     [2,2,1],
-#     [4,4,2]
-# ]
-# vec2 = [
-#     [1,1,0.5],
-#     [2,2,1]
-# ]
-
 def normalize(vectors):
     vec_length = np.linalg.norm(vectors, axis=1)
     avg_length = np.mean(vec_length)
     normalized_vector = vectors/avg_length
     return normalized_vector
-
-# print(normalize(vec1))
-# print(normalize(vec2))
