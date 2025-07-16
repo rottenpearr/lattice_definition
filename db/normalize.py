5d3d1f1277b747c4fd13b0cc081dfff753fcf2c1
A1r3t0 <113704274+A1r3t0@users.noreply.github.com>
2025-07-16 19:52:50 +0500

    deleted normalize.py

--- db/normalize.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-def normalize(vectors):
-    vec_length = np.linalg.norm(vectors, axis=1)
-    avg_length = np.mean(vec_length)
-    normalized_vector = vectors/avg_length
-    return normalized_vector
