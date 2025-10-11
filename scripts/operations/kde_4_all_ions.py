4524ad09e675692d2b038dc3ada17189d8fa1056
A1r3t0 <113704274+A1r3t0@users.noreply.github.com>
2025-10-11 14:32:37 +0500

    Добавлен импорт метода генерации шумовых координат, закомментирован метод для создания идеальных (эталонных) координат для шума

--- scripts/operations/kde_4_all_ions.py
+++ scripts/operations/kde_4_all_ions.py
@@ -3,14 +3,16 @@ from scripts.operations.spectrum_operations import kde_array
 from collections import Counter
 from os.path import normpath, join, dirname
 
-from scripts.operations.testing import xyz_to_normalized_coords
+from scripts.operations.testing import xyz_to_normalized_coords, xyz_to_normalized_coords_with_noise
 
-path_to_xyz = normpath(join(dirname(__file__), '..', '..', 'data', 'xyz', '1523015_UN2.xyz'))
-a = xyz_to_normalized_coords(path_to_xyz)
+path_to_xyz = normpath(join(dirname(__file__), '..', '..', 'data', 'xyz', '1539157_U2N3.xyz'))
+# a = xyz_to_normalized_coords(path_to_xyz) # нормализованные координаты (идеальные без шума)
+
+b = xyz_to_normalized_coords_with_noise(path_to_xyz) # коорды с шумом
 
 substance_id = 777
 
-res = get_lattice_vectors3(a)
+res = get_lattice_vectors3(b)
 
 kde_arrays = {}
 
